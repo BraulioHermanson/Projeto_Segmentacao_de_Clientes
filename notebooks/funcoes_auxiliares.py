b455deb6d126500c6f9d24c3c77b015f8a717cca
BraulioHermanson <brauliohcampos0@outlook.com>
2024-03-31 12:20:26 -0300

    commit new function add to the analysis

--- notebooks/funcoes_auxiliares.py
+++ notebooks/funcoes_auxiliares.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.colors import ListedColormap
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
@@ -32,4 +33,45 @@ def graficos_elbow_silhouette(X,random_state=42, intervalo_k=(2,11)):
     axs[1].set_xlabel("Silhouette Score")
     axs[1].set_title("Silhouette Method")
 
+    plt.show()
+
+
+def visualizar_clusters(
+    dataframe,
+    colunas,
+    quantidade_cores,
+    centroids,
+    mostrar_centroids=True, 
+    mostrar_pontos=False,
+    coluna_clusters=None,
+):
+
+    fig = plt.figure()
+    
+    ax = fig.add_subplot(111, projection="3d")
+    
+    cores = plt.cm.tab10.colors[:quantidade_cores]
+    cores = ListedColormap(cores)
+
+    x = dataframe[colunas[0]]
+    y = dataframe[colunas[1]]
+    z = dataframe[colunas[2]]
+    
+    ligar_centroids = mostrar_centroids
+    ligar_pontos = mostrar_pontos
+    
+    for i, centroid in enumerate(centroids):
+        if ligar_centroids: 
+            ax.scatter(*centroid, s=500, alpha=0.5)
+            ax.text(*centroid, f"{i}", fontsize=20, horizontalalignment="center", verticalalignment="center")
+    
+        if ligar_pontos:
+            s = ax.scatter(x, y, z, c=coluna_clusters, cmap=cores)
+            ax.legend(*s.legend_elements(), bbox_to_anchor=(1.3, 1))
+    
+    ax.set_xlabel(colunas[0])
+    ax.set_ylabel(colunas[1])
+    ax.set_zlabel(colunas[2])
+    ax.set_title("Clusters")
+    
     plt.show()
